67ecb74dcb75e32a580993fec11f09a366a19ce2
dranzerAvi <aviralhtc2015@gmail.com>
2020-07-28 10:07:49 +0530

    Gave it my best guys😵

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:io' as io;
 import 'package:encryptionapp/Encryption.dart';
+import 'package:encryptionapp/video_player.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permissions_plugin/permissions_plugin.dart';
 
 void main() {
@@ -34,6 +37,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //mahcode
+  Future<File> moveFile2(path, filename, destFolder) async {
+    print('start');
+    File sourceFile = File(path);
+    print('start1');
+    final checkPathExistence =
+        await Directory('storage/emulated/0/EncryptedFiles/$destFolder')
+            .exists();
+    if (!checkPathExistence) {
+      var testdir = await new io.Directory(
+              'storage/emulated/0/EncryptedFiles/$destFolder')
+          .create(recursive: true);
+    } else {
+      print('Pehle se hi hai');
+    }
+
+    try {
+      // prefer using rename as it is probably faster
+      print('end1');
+      return await sourceFile.rename(
+          'storage/emulated/0/EncryptedFiles/$destFolder/$filename.aes');
+    } on FileSystemException catch (e) {
+      print(e);
+
+      final newFile = await sourceFile
+          .copy('storage/emulated/0/EncryptedFiles/$destFolder/$filename.aes');
+      print('end');
+      await sourceFile.delete();
+      return newFile;
+    }
+  }
+
+  void encryptfileforfolder(path, String destFolder) async {
+    print('File path recieved $path');
+
+    var name = basename(path);
+    String encryptedFilePath = EncryptData.encrypt_file(path);
+    print(encryptedFilePath);
+    moveFile2(encryptedFilePath, name, destFolder);
+  }
+
+  void _listofFilesfromFolder(String folderPath) async {
+    directory = (await getApplicationDocumentsDirectory()).path;
+    setState(() {
+      files = io.Directory("storage/emulated/0/$folderPath/")
+          .listSync(); //use your folder name insted of resume.
+    });
+
+    print(files.runtimeType);
+  }
+
+  //Declare Globaly
+  String directory;
+  List file = new List();
+  List files = new List();
+
+  bool statusOfFolderFilesEncryption = false;
+  // Make New Function
+  void _listofFiles() async {
+    directory = (await getApplicationDocumentsDirectory()).path;
+    setState(() {
+      file = io.Directory("storage/emulated/0/EncryptedFiles/")
+          .listSync(); //use your folder name insted of resume.
+    });
+
+    print(file.runtimeType);
+  }
+
+  //mahCodeEnds😛
+
   List<String> imageUrls = [];
 
   String _path;
@@ -44,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<String> openFileExplorer() async {
     try {
       _path = null;
-      _paths = await FilePicker.getMultiFilePath(type: FileType.image);
+      _paths = await FilePicker.getMultiFilePath(type: FileType.video);
 
       encryptFiles();
     } on PlatformException catch (e) {
@@ -60,7 +133,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    // TODO: implement initState
+    super.initState();
     request();
+    _listofFiles();
+    print(
+        '\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$ ${file.length}\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$');
   }
 
   void request() async {
@@ -81,14 +159,29 @@ class _MyHomePageState extends State<MyHomePage> {
     moveFile(encryptedFilePath, name);
   }
 
-  Future<String> decryptfile() async {
+  Future<String> decryptfile(BuildContext context) async {
     File file = await FilePicker.getFile();
     String filepath = file.path;
     print('File path recieved $filepath');
     String spath = filepath.substring(1, filepath.length);
     print('File slashed path is $spath');
-    String decryptedFilePath = EncryptData.decrypt_file(spath);
-    print(decryptedFilePath);
+    String decryptedFilePath = await EncryptData.decrypt_file(spath);
+    await print(
+        '-----------------------${decryptedFilePath}-----------------------------');
+    File filePlayed =
+        await File('storage/emulated/0/EncryptedFiles/Snapchat-415885974.mp4');
+
+    if (filePlayed != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => VideoPlayer(
+            video: filePlayed,
+          ),
+        ),
+      );
+    } else {}
+
     Fluttertoast.showToast(
         msg:
             'Encryption successful.\nEncrypted file path : $decryptedFilePath');
@@ -115,8 +208,13 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  String destFolderPath;
+  TextEditingController tc = new TextEditingController();
+  TextEditingController tc2 = new TextEditingController();
   @override
   Widget build(BuildContext context) {
+    _listofFiles();
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
@@ -127,6 +225,49 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: tc,
+                  ),
+                )),
+            InkWell(
+              onTap: () async {
+                statusOfFolderFilesEncryption = false;
+                await _listofFilesfromFolder(tc.text);
+                destFolderPath = tc2.text;
+                for (int i = 0; i < files.length; i++) {
+                  encryptfileforfolder(files[i].path, destFolderPath);
+                }
+                await setState(() {
+                  print('ho gaya');
+                });
+                statusOfFolderFilesEncryption = true;
+              },
+              child: Card(
+                  color: Colors.black54,
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Text(
+                      'Encrypt all in the folder',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+            ),
+            Container(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: tc2,
+                  ),
+                )),
+
             InkWell(
               onTap: () {
                 openFileExplorer().then((value) => _scaffoldKey.currentState
@@ -148,7 +289,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             InkWell(
               onTap: () {
-                decryptfile().then((value) => _scaffoldKey.currentState
+                decryptfile(context).then((value) => _scaffoldKey.currentState
                     .showSnackBar(
                         SnackBar(content: Text('Successfully Decrypted'))));
               },
@@ -160,11 +301,128 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Padding(
                     padding: const EdgeInsets.all(18.0),
                     child: Text(
-                      'Decrypt files',
+                      'Decrypt file',
                       style: TextStyle(color: Colors.white),
                     ),
                   )),
             ),
+            InkWell(
+              onTap: () async {
+                File filePlayed;
+                filePlayed = await File('/storage/emulated/0/Test/Hi.mp4');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => VideoPlayer(
+                      video: filePlayed,
+                    ),
+                  ),
+                );
+              },
+              child: Card(
+                  color: Colors.black54,
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Text(
+                      'Play a video',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      // your Content if there
+                      Container(
+                        color: Colors.black.withOpacity(0.3),
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Folder Contents',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                            itemCount: file.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return file[index].toString().contains('.aes') ||
+                                      (!file[index].toString().contains('.'))
+                                  ? InkWell(
+                                      onTap: () {
+                                        if (!basename(
+                                                file[index].toString().trim())
+                                            .toString()
+                                            .substring(
+                                                0,
+                                                basename(file[index]
+                                                            .toString()
+                                                            .trim())
+                                                        .toString()
+                                                        .length -
+                                                    1)
+                                            .contains('.aes')) {}
+                                      },
+                                      child: Card(
+                                          child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(basename(
+                                                file[index].toString().trim())
+                                            .toString()
+                                            .substring(
+                                                0,
+                                                basename(file[index]
+                                                            .toString()
+                                                            .trim())
+                                                        .toString()
+                                                        .length -
+                                                    1)),
+                                      )),
+                                    )
+                                  : Container();
+                            }),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+//            InkWell(
+//              onTap: () async {
+//                File file = await FilePicker.getFile();
+//                String filepath = file.path;
+//                print('File path recieved $filepath');
+//                File filePlayed = File(filepath);
+//                Navigator.push(
+//                  context,
+//                  MaterialPageRoute(
+//                    builder: (context) => VideoPlayer(
+//                      video: filePlayed,
+//                    ),
+//                  ),
+//                );
+//              },
+//              child: Card(
+//                  color: Colors.black54,
+//                  elevation: 10,
+//                  shape: RoundedRectangleBorder(
+//                      borderRadius: BorderRadius.all(Radius.circular(10))),
+//                  child: Padding(
+//                    padding: const EdgeInsets.all(18.0),
+//                    child: Text(
+//                      'Play',
+//                      style: TextStyle(color: Colors.white),
+//                    ),
+//                  )),
+//            ),
           ],
         ),
       ),
---
+++ lib/video_player.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayer extends StatefulWidget {
+  File video;
+
+  VideoPlayer({this.video});
+
+  @override
+  _VideoPlayerState createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(widget.video)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(),
+                )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
